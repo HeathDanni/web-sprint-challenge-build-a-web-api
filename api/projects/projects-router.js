66e3e1d79c5c44a3813e6bf0b3b6fb40c01c2b49
Heather Beckman <heatherbeckman85@gmail.com>
2020-12-12 07:43:40 -0700

    added middleware

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,49 +1,63 @@
 // Write your "projects" router here!
 const express = require("express")
-const projectsModel = require("./projects-model")
+const projects = require("./projects-model")
 
 const router = express.Router()
 
-router.get("/api/projects", (req, res, next) => {
-    projectsModel.get()
-        .then((actions) => res.status(200).json(actions))
-        .catch((err) => next(err))
+router.get("/api/projects", (req, res) => {
+    projects.get()
+        .then((project) => res.status(200).json(project))
+        .catch((err) => res.status(500).json("there was an error"))
 })
 
-router.get("/api/projects/:id", (req, res, next) => {
-    projectsModel.get(req.params.id)
-        .then((project) => res.status(200).json(project))
-        .catch((err) => next(err))
+router.get("/api/projects/:id", validateProject(), (req, res) => {
+    res.status(200).json(req.project)
 })
 
 router.post("/api/projects", (req, res) => {
-    projectsModel.insert(req.body)
+    projects.insert(req.body)
         .then((project) => res.status(201).json(project))
-        .catch((err) => next(err))
+        .catch((err) => res.status(500).json("there was an error"))
 })
 
-router.put("/api/projects/:id", (req, res, next) => {
-    projectsModel.update(req.params.id, req.body)
+router.put("/api/projects/:id", validateProject(), (req, res) => {
+    projects.update(req.project, req.body)
         .then((project) => res.status(201).json(project))
-        .catch((err) => next(err))
+        .catch((err) => res.status(500).json("there was an error"))
 })
 
-router.delete("/api/projects/:id", (req, res, next) => {
-    projectsModel.remove(req.params.id)
+router.delete("/api/projects/:id", validateProject(), (req, res) => {
+    projects.remove(req.project)
         .then((project) => res.status(200).json(project))
-        .catch((err) => next(err))
+        .catch((err) => res.status(500).json("there was an error"))
 })
 
-router.get("/api/projects/:projectId/actions", (req, res, next) => {
-    projectsModel.getProjectActions(req.params.projectId)
-        .then((actions) => res.status(200).json(actions))
-        .catch((err) => next(err))
+router.get("/api/projects/:id/actions", validateProject(), (req, res) => {
+    projects.getProjectActions(req.project)
+        .then((project) => res.status(200).json(project))
+        .catch((err) => res.status(500).json("there was an error"))
 })
 
-// function validateActionId() {
-//     return (req, res, next) => {
-//         actionsModel.find
-//     }
-// }
+function validateProject() {
+    return (req, res, next) => {
+        projects.get(req.params.id)
+            .then((project) => {
+                if (project) {
+                    req.project = project
+                    next()
+                } else {
+                    res.status(404).json({
+                        message: "Project does not exist"
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        res.status(500).json({
+                            message: "There was an error retrieving project"
+                        })
+                    })
+                }
+            })
+        }
+}
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -8,11 +8,4 @@ server.use(express.json())
 server.use(actionsRouter)
 server.use(projectsRouter)
 
-// server.use((err, req, res, next) => {
-//     console.log(err)
-//     res.status(500).json({
-//         message: "Something went wrong"
-//     })
-// })
-
 module.exports = server;
