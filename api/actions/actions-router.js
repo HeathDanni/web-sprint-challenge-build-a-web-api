41d6e96a0ae492a3df4a07549a28a18af2fd6dc3
Heather Beckman <heatherbeckman85@gmail.com>
2020-12-12 05:57:04 -0700

    got server running

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,35 @@
 // Write your "actions" router here!
+const express = require("express")
+const actionsModel = require("./actions-model")
+
+const router = express.Router()
+
+router.get("/api/actions", (req, res) => {
+    actionsModel.get()
+        .then((actions) => res.status(200).json(actions))
+        .catch((err) => next(err))
+})
+
+router.get("/api/actions/:id", (req, res) => {
+    
+})
+
+router.post("/api/actions", (req, res) => {
+    
+})
+
+router.put("/api/actions/:id", (req, res) => {
+    
+})
+
+router.delete("/api/actions/:id", (req, res) => {
+    
+})
+
+// function validateActionId() {
+//     return (req, res, next) => {
+//         actionsModel.find
+//     }
+// }
+
+module.exports = router
